bd58ad7e48df960096f806d166bfad192cf9e87f
zcraig <zcraig@my.waketech.edu>
2026-04-06 23:09:48 -0400

    Updated Main Village Backgrounds to Reflect Village Infection %
    - Added Healthy and Mid-Health Village backgrounds in addition to the Toxic Village background.
    - The main background dynamically responds to the Village Infection rate.
    - Added mid health and healthy `villager1` images too, so he changes with the village ( will add functionality for this later )
    - When villagers objective is generated in the Arrival Scene, villagers are randomly selected to be the ones to be healed in that playthrough.
    - Moved static villager info like image, name, background, etc into `game-data/villagers.js`, while dynamic info about them stays in `gameStatejs`.

--- Plague Village/game-data/villagers.js
+++ Plague Village/game-data/villagers.js
@@ -1,2 +1,73 @@
 // Villager types will be added here later
-const villagerDatabase = {};
+const villagerDatabase = {
+
+    villager1: {
+        name: "Villager Name 1",
+        bg: "images/backgrounds/villager-home-1.png",
+        portrait_sick: "images/characters/villager1-sick.png",
+        portrait_healthy: "images/characters/villager1-healthy.png"
+    },
+
+    villager2: {
+        name: "Villager Name 2",
+        bg: "images/backgrounds/villager-home-2.png",
+        portrait_sick: "images/characters/villager2-sick.png",
+        portrait_healthy: "images/characters/villager2-healthy.png"
+    },
+
+    villager3: {
+        name: "Villager Name 3",
+        bg: "images/backgrounds/villager-home-3.png",
+        portrait_sick: "images/characters/villager3-sick.png",
+        portrait_healthy: "images/characters/villager3-healthy.png"
+    },
+
+    villager4: {
+        name: "Villager Name 4",
+        bg: "images/backgrounds/villager-home-4.png",
+        portrait_sick: "images/characters/villager4-sick.png",
+        portrait_healthy: "images/characters/villager4-healthy.png"
+    },
+
+    villager5: {
+        name: "Villager Name 5",
+        bg: "images/backgrounds/villager-home-5.png",
+        portrait_sick: "images/characters/villager5-sick.png",
+        portrait_healthy: "images/characters/villager5-healthy.png"
+    },
+
+    villager6: {
+        name: "Villager Name 6",
+        bg: "images/backgrounds/villager-home-6.png",
+        portrait_sick: "images/characters/villager6-sick.png",
+        portrait_healthy: "images/characters/villager6-healthy.png"
+    },
+
+    villager7: {
+        name: "Villager Name 7",
+        bg: "images/backgrounds/villager-home-7.png",
+        portrait_sick: "images/characters/villager7-sick.png",
+        portrait_healthy: "images/characters/villager7-healthy.png"
+    },
+
+    villager8: {
+        name: "Villager Name 8",
+        bg: "images/backgrounds/villager-home-8.png",
+        portrait_sick: "images/characters/villager8-sick.png",
+        portrait_healthy: "images/characters/villager8-healthy.png"
+    },
+
+    villager9: {
+        name: "Villager Name 9",
+        bg: "images/backgrounds/villager-home-9.png",
+        portrait_sick: "images/characters/villager9-sick.png",
+        portrait_healthy: "images/characters/villager9-healthy.png"
+    },
+
+    villager10: {
+        name: "Villager Name 10",
+        bg: "images/backgrounds/villager-home-10.png",
+        portrait_sick: "images/characters/villager10-sick.png",
+        portrait_healthy: "images/characters/villager10-healthy.png"
+    }
+}
