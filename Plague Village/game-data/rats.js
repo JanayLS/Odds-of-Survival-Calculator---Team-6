f34f1168846fe2f08b37f6aec2a1c7fb1ab88fdb
Noah Yarosz <ndyarosz@my.waketech.edu>
2026-04-19 19:14:05 -0400

    Update rat images and background paths

--- Plague Village/game-data/rats.js
+++ Plague Village/game-data/rats.js
@@ -4,32 +4,32 @@ const ratsDatabase = {
 
     rat1: {
         name: "Rat 1",
-        bg: "images/backgrounds/rat-background-1.png",
-        img: "",
+        bg: "images/backgrounds/rat-backgrounds/rat-background-1.png",
+        img: "images/characters/rats/rat1.png",
     },
 
     rat2: {
         name: "Rat 2",
-        bg: "",
-        img: ""
+        bg: "images/backgrounds/rat-backgrounds/rat-background-2.png",
+        img: "images/characters/rats/rat2.png",
     },
 
     rat3: {
         name: "Rat 3",
-        bg: "",
-        img: ""
+        bg: "images/backgrounds/rat-backgrounds/rat-background-3.png",
+        img: "images/characters/rats/rat3.png"
     },
 
     rat4: {
         name: "Rat 4",
-        bg: "",
-        img: ""
+        bg: "images/backgrounds/rat-backgrounds/rat-background-4.png",
+        img: "images/characters/rats/rat4.png"
     },
 
     rat5: {
         name: "Rat 5",
-        bg: "",
-        img: ""
+        bg: "images/backgrounds/rat-backgrounds/rat-background-5.png",
+        img: "images/characters/rats/rat5.png"
     }
 
 };
